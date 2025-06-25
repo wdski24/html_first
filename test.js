a13aa136890361911b549069ef64df734a79677e
wdski <ws23@jonas-heuer.com>
2025-06-25 15:53:55 +0200

    more Information added

--- test.js
+++ test.js
@@ -6,10 +6,10 @@
 
 //Datentypen und Variablen
 
-//Alt: sowohl constante als auch varibale
+//Alt: sowohl constante als auch varibale (initialisierung ohne Zuweisung immer am Anfang des Dokuments)
 var a = 'variabel';
 
-//Neu: let ist variabel const ist constant
+//Neu: let ist variabel const ist constant (initialisierung zur Laufzeit an entsprechender Stelle)
 let b = 'varibale';
 const c = 'constant';
 
@@ -25,8 +25,14 @@ let floatArrayVar = [1.2, 2.3, 3.5];
 let stringArrayVar = ['eins', 'zwei', 'drei'];
 
 //Object
-
+// Objekte bestehen aus sogenannten Key-Value Pairs in folgender Notation:
+// key: value -> der key ist der eindeutige Identifier und unabhängig vom Datentypen (technischer Name), der Value repräsentiert die zugrundeliegenden Daten
+let objectName = {
+  name: 'Name',
+  firstName: 'First Name',
+  yearOfStudy: 2024,
+};
 //Class
 
 //Ausgabe in der Konsole zur Validierung von Daten
-console.log(b);
+console.log(objectName.name);
