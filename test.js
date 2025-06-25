a977d3b18f0121a0b06a0d9dd69f4116001aa8ca
wdski <ws23@jonas-heuer.com>
2025-06-25 15:32:35 +0200

    first data types js

--- test.js
+++ test.js
@@ -1,1 +1,32 @@
-var test = 'test';
+//Zeilenkommentare
+/*
+ * Mehrzeiliger
+ * Kommentar
+ */
+
+//Datentypen und Variablen
+
+//Alt: sowohl constante als auch varibale
+var a = 'variabel';
+
+//Neu: let ist variabel const ist constant
+let b = 'varibale';
+const c = 'constant';
+
+//String
+let stringVar = 'string';
+//Number
+let intVar = 1;
+let floatVar = 1.2;
+
+//Arrays
+let intArrayVar = [1, 2, 3];
+let floatArrayVar = [1.2, 2.3, 3.5];
+let stringArrayVar = ['eins', 'zwei', 'drei'];
+
+//Object
+
+//Class
+
+//Ausgabe in der Konsole zur Validierung von Daten
+console.log(b);
