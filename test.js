ce784c9ea22e0975addea40a498116cabe1b3e56
wdski <ws23@jonas-heuer.com>
2025-06-26 09:45:03 +0200

    functions and classes added

--- test.js
+++ test.js
@@ -6,7 +6,7 @@
 
 //Datentypen und Variablen
 
-//Alt: sowohl constante als auch varibale (initialisierung ohne Zuweisung immer am Anfang des Dokuments)
+//Alt: sowohl constante als auch varibale (initialisierung ohne Zuweisung immer am Anfang des Dokuments = immer Global)
 var a = 'variabel';
 
 //Neu: let ist variabel const ist constant (initialisierung zur Laufzeit an entsprechender Stelle)
@@ -32,7 +32,30 @@ let objectName = {
   firstName: 'First Name',
   yearOfStudy: 2024,
 };
+
 //Class
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
+
+let rect1 = new Rectangle(12, 24);
+console.log(rect1);
+
+//Funktionen in JavaScript:
+//ES5: function FUNKTIONSNAME (param1, ...., paramN){}
+function add(value1, value2) {
+  return value1 + value2;
+}
+
+//ES6: Supported Arrow Functions: Eine Variable kann auch den "Datentyp" Funktion haben
+const sub = (value1, value2) => {
+  return value1 - value2;
+};
+//Syntactic Sugaring: Funktion und Funktionsweise identisch zu vorheriger Funktion
+//const sub = (value1, value2) => value1 - value2;
 
 //Ausgabe in der Konsole zur Validierung von Daten
 console.log(objectName.name);
